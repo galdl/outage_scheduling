3297ef5a9ecf57d54d26695df30d05d3db38af40
galdl <galdl20@gmail.com>
2016-07-13 19:16:00 +0300

    NN optimize, until sendJob, is running smooth

---
+++ src/.fuse_hidden000059b80000001b
@@ -0,0 +1,6 @@
+function  [argContentFilename] = perpareJobDir(localIterDir,i_job,GENERAL_JOB_FILENAME,params)
+%% write input file
+% argContentFilename=[GENERAL_JOB_FILENAME,'_n_',num2str(i_job)];
+argContentFilename = GENERAL_JOB_FILENAME;
+save([localIterDir,'/',argContentFilename],'params');
+
--- src/perpareJobDir.m
+++ src/perpareJobDir.m
@@ -1,6 +1,6 @@
-function  [argContentFilename] = perpareJobDir(localIterDir,i_job,GENERAL_JOB_FILENAME,params)
+function  [argContentFilename] = perpareJobDir(localIterDir,i_job,GENERAL_JOB_FILENAME,params,config)
 %% write input file
 % argContentFilename=[GENERAL_JOB_FILENAME,'_n_',num2str(i_job)];
 argContentFilename = GENERAL_JOB_FILENAME;
-save([localIterDir,'/',argContentFilename],'params');
+save([localIterDir,'/',argContentFilename],'params','config');
 
