6b4e7167971496b78dd482b906ba3f67017c7ba7
galdl <galdl20@gmail.com>
2016-07-28 16:17:35 +0300

    before changing to outage scheduling combinatorics

--- src/restoreSplitData.m
+++ src/restoreSplitData.m
@@ -4,10 +4,11 @@ final_db = [];
 sample_matrix = [];
 
 list = what(split_data_loc);
-num_data_chunks = length(list.mat);
-
-for i_chunk=1:num_data_chunks
-    load([split_data_loc,'/data_chunk_',num2str(i_chunk),'.mat'],'curr_sample_mat','curr_final_db');
-    sample_matrix=[sample_matrix,curr_sample_mat];
-    final_db = [final_db;curr_final_db];
+    
+for i_file=1:length(list.mat)
+    if(regexp(list.mat{i_file},'data_chunk')==1)
+        load([split_data_loc,'/',list.mat{i_file}],'curr_sample_mat','curr_final_db');
+        sample_matrix=[sample_matrix,curr_sample_mat];
+        final_db = [final_db;curr_final_db];
+    end
 end
